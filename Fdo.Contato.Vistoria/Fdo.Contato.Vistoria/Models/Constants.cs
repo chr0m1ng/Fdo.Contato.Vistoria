ba14209a6bdc0a075938bb862cb67ea67b17038d
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-07-18 23:27:33 -0300

    Add: settings constants

--- Fdo.Contato.Vistoria/Fdo.Contato.Vistoria/Models/Constants.cs
+++ Fdo.Contato.Vistoria/Fdo.Contato.Vistoria/Models/Constants.cs
@@ -3,5 +3,10 @@
     public static class Constants
     {
         public const string MAIN_TITLE = "Contato Vistoria Itabuna";
+        public const string SETTINTGS_TITLE = "Configurações";
+        public const string SETTINGS_HOST_KEY = "hostname";
+        public const string SETTINGS_AUTH_KEY = "apikey";
+        public const string SETTINGS_HOST_DEFAULT_VALUE = "http://192.168.0.16:5000";
+        public const string SETTINGS_AUTH_DEFAULT_VALUE = "padrao";
     }
 }
