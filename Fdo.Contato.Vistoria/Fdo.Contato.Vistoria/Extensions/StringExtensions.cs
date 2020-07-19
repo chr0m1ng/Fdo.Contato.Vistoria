35cf055d52264e67482f1c0be0a348bd571e43ea
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-07-18 21:39:31 -0300

    Add: string extension to get filename

---
+++ Fdo.Contato.Vistoria/Fdo.Contato.Vistoria/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Fdo.Contato.Vistoria.Extensions
+{
+    public static class StringExtensions
+    {
+        private const string PLATE_REGEX = @"[A-Z]{3}-[0-9][0-9A-Z][0-9]{2}";
+
+        public static Task<bool> IsPlateAsync(this string value)
+        {
+            return Task.FromResult(Regex.IsMatch(value, PLATE_REGEX));
+        }
+
+        public static string GetFileName(this string imagePath)
+        {
+            return Path.GetFileName(imagePath);
+        }
+    }
+}
