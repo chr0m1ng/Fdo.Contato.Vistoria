f1428b299e3e31931e5ae1858cbddbfab773e92f
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-07-18 21:40:30 -0300

    Add: ObervableCollection with AddRange method

---
+++ Fdo.Contato.Vistoria/Fdo.Contato.Vistoria/Models/ExtendedObservableCollection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Fdo.Contato.Vistoria.Models
+{
+    public class ExtendedObservableCollection<T> : ObservableCollection<T>
+    {
+        public void AddRange(IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                Add(item);
+            }
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, items));
+        }
+    }
+}
