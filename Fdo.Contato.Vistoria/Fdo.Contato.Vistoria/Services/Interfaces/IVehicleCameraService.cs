c9c671af1de2eccdc0923ad08d50b5a38f6a534f
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-07-18 21:43:53 -0300

    Add: camera service using CrossMedia plugin

---
+++ Fdo.Contato.Vistoria/Fdo.Contato.Vistoria/Services/Interfaces/IVehicleCameraService.cs
@@ -0,0 +1,13 @@
+﻿using Fdo.Contato.Vistoria.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fdo.Contato.Vistoria.Services.Interfaces
+{
+    public interface IVehicleCameraService
+    {
+        Task<VehicleImage> TakePhotoAsync(string plate);
+
+        Task<IEnumerable<VehicleImage>> GetGalleryVehiclePhotosAsync();
+    }
+}
---
+++ Fdo.Contato.Vistoria/Fdo.Contato.Vistoria/Services/VehicleCameraService.cs
@@ -0,0 +1,56 @@
+﻿using Fdo.Contato.Vistoria.Extensions;
+using Fdo.Contato.Vistoria.Models;
+using Fdo.Contato.Vistoria.Services.Interfaces;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fdo.Contato.Vistoria.Services
+{
+    public class VehicleCameraService : IVehicleCameraService
+    {
+        public async Task<IEnumerable<VehicleImage>> GetGalleryVehiclePhotosAsync()
+        {
+            var pickMediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Full
+            };
+
+            var multiPickerOptions = new MultiPickerOptions()
+            {
+                AlbumSelectTitle = "Albuns",
+                BackButtonTitle = "Cancelar",
+                PhotoSelectTitle = "Selecionar fotos",
+                DoneButtonTitle = "Adicionar fotos"
+            };
+
+            var medias = await CrossMedia.Current.PickPhotosAsync(pickMediaOptions, multiPickerOptions);
+
+            if (medias == null)
+            {
+                return null;
+            }
+            return medias.Select(m => new VehicleImage() { Image = m, Name = m.Path.GetFileName() });
+        }
+
+        public async Task<VehicleImage> TakePhotoAsync(string plate)
+        {
+            var storeCameraMediaOptions = new StoreCameraMediaOptions()
+            {
+                AllowCropping = true,
+                SaveToAlbum = true,
+                Directory = plate
+            };
+
+            var media = await CrossMedia.Current.TakePhotoAsync(storeCameraMediaOptions);
+
+            if (media == null)
+            {
+                return null;
+            }
+            return new VehicleImage() { Name = media.Path.GetFileName(), Image = media };
+        }
+    }
+}
